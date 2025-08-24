fdac79291e14a902ed9fe8370a9f6671e2011bd1
francisco martín <frga.martin@alumno.um.edu.ar>
2025-08-24 19:59:46 -0300

    Generacion del tablero con los espacios para guardar fichas comidas y fichas fuera de juego

--- core/src/board.py
+++ core/src/board.py
@@ -1,2 +1,87 @@
-class board:
-    pass
+import unittest
+
+class Board:
+    def __init__(self):
+        self.__casillas__ = [
+            ['white','white'], #0
+            [], #1
+            [], #2
+            [], #3
+            [], #4
+            ['black', 'black', 'black', 'black', 'black'], #5
+            #--------------------------------------------
+            [], #6
+            ['black', 'black', 'black'], #7
+            [], #8
+            [], #9
+            [], #10
+            ['white', 'white', 'white', 'white', 'white'], #11
+            #--------------------------------------------
+            ['black', 'black', 'black', 'black', 'black'], #12
+            [], #13
+            [], #14
+            [], #15
+            ['white', 'white', 'white'], #16
+            [], #17
+            #--------------------------------------------
+            ['white', 'white', 'white', 'white', 'white'], #18
+            [], #19
+            [], #20
+            [], #21
+            [], #22
+            ['black', 'black'], #23
+        ] #Definimos el tablero general de 24 casillas como una lista
+
+        self.__banco__ = {"white": [], "black": []} #Lugar donde guardamos las fichas comidas
+
+        self.__home__ = {"white": [], "black": []} #Lugar donde guardamos las fichas al finalizar el juego
+
+class TestBoardInitialization(unittest.TestCase):
+
+    def setUp(self):
+        self.board = Board()
+
+    def test_tablero_tiene_24_casillas(self):
+        """El tablero debe tener exactamente 24 casillas"""
+        self.assertEqual(len(self.board.__casillas__), 24)
+
+    def test_configuracion_inicial_casilla_0(self):
+        """La casilla 0 debe comenzar con 2 fichas blancas"""
+        self.assertEqual(self.board.__casillas__[0], ['white','white'])
+
+    def test_configuracion_inicial_casilla_5(self):
+        """La casilla 5 debe comenzar con 5 fichas negras"""
+        self.assertEqual(self.board.__casillas__[5], ['black']*5)
+
+    def test_configuracion_inicial_casilla_11(self):
+        """La casilla 11 debe comenzar con 5 fichas blancas"""
+        self.assertEqual(self.board.__casillas__[11], ['white']*5)
+
+    def test_configuracion_inicial_casilla_12(self):
+        """La casilla 12 debe comenzar con 5 fichas negras"""
+        self.assertEqual(self.board.__casillas__[12], ['black']*5)
+
+    def test_configuracion_inicial_casilla_16(self):
+        """La casilla 16 debe comenzar con 3 fichas blancas"""
+        self.assertEqual(self.board.__casillas__[16], ['white']*3)
+
+    def test_configuracion_inicial_casilla_18(self):
+        """La casilla 18 debe comenzar con 5 fichas blancas"""
+        self.assertEqual(self.board.__casillas__[18], ['white']*5)
+
+    def test_configuracion_inicial_casilla_23(self):
+        """La casilla 23 debe comenzar con 2 fichas negras"""
+        self.assertEqual(self.board.__casillas__[23], ['black']*2)
+
+    def test_banco_empieza_vacio(self):
+        """El banco debe empezar vacío para ambos colores"""
+        self.assertEqual(self.board.__banco__['white'], [])
+        self.assertEqual(self.board.__banco__['black'], [])
+
+    def test_home_empieza_vacio(self):
+        """El home debe empezar vacío para ambos colores"""
+        self.assertEqual(self.board.__home__['white'], [])
+        self.assertEqual(self.board.__home__['black'], [])
+
+if __name__ == '__main__':
+    unittest.main()
