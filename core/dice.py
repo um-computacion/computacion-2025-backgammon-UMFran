ceabc949fce98a5c5f82f0a39b4b6453a2ea6ca1
francisco martín <frga.martin@alumno.um.edu.ar>
2025-08-31 21:32:17 -0300

    Creacion funcion usar dados

--- core/dice.py
+++ core/dice.py
@@ -12,3 +12,13 @@ class dice:
             self.__movimientos__ = [dado1] *4 #Si los dados son iguales se duplican los turnos
         else:
             self.__movimientos__ = [dado1, dado2] #Si los dados son distintos se agregan individualmente
+    
+    def get_dados(self):
+        return list(self.__movimientos__)
+
+    def usar_dado(self, valor: int):
+        
+        if valor not in self.__movimientos__:
+            raise ValueError(f"El valor {valor} no se encuentra entre los generados")
+        else:
+            self.__movimientos__.remove(valor) #Elimina el valor usado por el jugador
