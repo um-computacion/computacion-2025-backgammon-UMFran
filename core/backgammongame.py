3b505e8ead2b6c79da6e29cceec9633b5ee6037e
francisco martín <frga.martin@alumno.um.edu.ar>
2025-09-15 20:15:43 -0300

    Creacion de funciones para el juego

--- core/backgammongame.py
+++ core/backgammongame.py
@@ -3,12 +3,13 @@ from core.dice import dice
 from core.player import Player
 
 class backgammongame:
-    def __init__(self, jugador1, jugador2):
+    def __init__(self, jugador1: str, jugador2: str):
         self.__board__ = Board()
         self.__dados__ = dice()
         self.__jugador1__ = Player(jugador1, "white")
         self.__jugador2__ = Player(jugador2, "black")
-        self.__turno__ = jugador1
+        self.__jugadores__ = {self.__jugador1__, self.__jugador2__}
+        self.__turno__ = self.__jugador1__
         self.__turno_finalizado__ = False
 
 # Funciones básicas:
@@ -20,4 +21,26 @@ class backgammongame:
         return self.__jugador2__
     
     def mostrar_turno(self):
-        return self.__turno__
+        return self.__turno__.obtener_nombre()
+    
+#Funciones de dados
+
+    def tirar_dados(self):
+        self.__dados__.tirar_dados()
+        return self.__dados__.get_dados()
+    
+    def cambiar_turno(self):
+        if self.__turno__ == self.__jugador1__:
+            self.__turno__ = self.__jugador2__
+        else:
+            self.__turno__ = self.__jugador1__
+
+    def ganador(self):
+        for jugador in self.__jugadores__: 
+            if jugador.ganar():
+                self.__juego_terminado__ = True
+                return jugador.obtener_nombre()
+        return None
+    
+    def juego_terminado(self):
+        return self.__jugador1__.ganar() or self.__jugador2__.ganar()
