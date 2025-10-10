6c73faf8fd753c9240644ce988d81080f9ac0824
francisco martín <frga.martin@alumno.um.edu.ar>
2025-10-10 20:37:34 -0300

    Creacion de barra central y definir colores

--- pygame_ui/interfaz_tablero.py
+++ pygame_ui/interfaz_tablero.py
@@ -10,4 +10,18 @@ class TableroGrafico:
         self.alto_triangulo = self.alto // 2
         self.radio_ficha = self.ancho_triangulo // 3
     
-    
+    def dibujar_tablero(self):
+        colores= [(180, 100, 80), (250, 220, 200)] #Color triangulos
+
+        color_barra_central = (139, 69, 19)
+        color_borde = (60, 40, 30)
+        color_fondo = (220, 190, 160)
+        self.pantalla.fill(color_fondo)
+
+        #Barra central
+        x_barra = x_barra = (self.ancho / 2) - (self.ancho_barra / 2)
+        pygame_ui.draw.rect(self.pantalla,color_barra_central,
+            pygame_ui.Rect(x_barra, 0, self.ancho_barra, self.alto))
+        pygame_ui.draw.rect(self.pantalla,color_borde,
+            pygame_ui.Rect(x_barra, 0, self.ancho_barra, self.alto),2)
+        
